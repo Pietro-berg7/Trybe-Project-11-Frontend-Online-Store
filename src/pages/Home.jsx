caba759f11c2eed986d44b2ce074e5e4c5e86d41
Matheus Bezerra <mabezerra2@gmail.com>
2022-10-07 18:17:33 -0400

    feat(Page Home): renderização dos produtos
    o botão de pesquisa para pesquisar produtos relacionados e ser renderizado na Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import Loading from '../components/Loading';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import Category from '../components/Category';
 
 export default class Home extends Component {
   state = {
+    listProduct: [],
     category: [],
+    product: false,
+    search: '',
   };
 
   componentDidMount() {
@@ -20,21 +23,94 @@ export default class Home extends Component {
     });
   };
 
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      search: value,
+    });
+  };
+
+  handleClickButton = () => {
+    const { search } = this.state;
+    if (search.trim() === '') {
+      this.setState({
+        listProduct: [],
+        product: false,
+      });
+      return;
+    }
+    getProductsFromCategoryAndQuery(null, search)
+      .then(({ results }) => {
+        this.setState({
+          listProduct: results, product: true,
+        });
+      });
+  };
+
   render() {
-    const { category } = this.state;
+    const { category, listProduct, product } = this.state;
     return (
       <main>
-        {category.map(({ name, id }) => (
-          <Category
-            key={ id }
-            name={ name }
-            id={ id }
-          />
-        ))}
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <aside>
+          {category.map(({ name, id }) => (
+            <Category
+              key={ id }
+              name={ name }
+              id={ id }
+            />
+          ))}
+        </aside>
+        <setion>
+          <label htmlFor="search">
+            <input
+              name="search"
+              id="search"
+              type="text"
+              data-testid="query-input"
+              onChange={ this.handleChange }
+              placeholder="Pesquisa"
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ this.handleClickButton }
+            data-testid="query-button"
+          >
+            Buscar
+          </button>
+        </setion>
         <Link to="/shoppingcart" data-testid="shopping-cart-button">Clique aqui</Link>
+        {listProduct.length === 0 && !product
+          ? (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )
+          : (
+            <ul>
+              {listProduct.map(({ id, title, price, thumbnail }) => (
+                <li
+                  key={ id }
+                  data-testid="product"
+                >
+                  <Link
+                    to={ `/productdetails/${id}` }
+                    data-testid="product-detail-link"
+                  >
+                    btn
+                  </Link>
+                  <p>{title}</p>
+                  <img src={ thumbnail } alt={ title } />
+                  <p>{price}</p>
+                  <button
+                    type="submit"
+                  >
+                    Adicionar
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        {product || <p>Nenhum produto foi encontrado</p>}
       </main>
     );
   }
