96fab0dfbe158480be9e3dec35a3fb68922d7bd5
Matheus Bezerra <mabezerra2@gmail.com>
2022-10-07 16:07:14 -0400

    feat(Page ShoppingCart): foi criado component Shoppingcart
    foi criado Page ShoppingCart e feito as Rotas para o mesmo.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Home } />
+            <Route exact path="/shoppingcart" component={ ShoppingCart } />
           </Switch>
         </BrowserRouter>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 // import Loading from '../components/Loading';
 
 export default class Home extends Component {
@@ -14,6 +15,7 @@ export default class Home extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">Clique aqui</Link>
       </main>
     );
   }
